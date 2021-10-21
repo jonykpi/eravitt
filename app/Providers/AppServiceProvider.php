3c920bc93b962dc92c4be6d661089ce5ead5199b
jony <djony620@gmail.com>
2021-10-21 10:26:35 +0600

    modification

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -31,7 +31,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        
+        \URL::forceScheme('https');
         if($this->app->environment() === 'production'){
             $this->app['request']->server->set('HTTPS', true);
         }
